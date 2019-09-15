b556dacf587d0dc6291d9c1b2a8a38abfa6de559
Bruno Santos <brunosantos@protonmail.com>
2019-09-14 22:08:31 -0300

    user model, database url, install sqlalchemy

--- core.py
+++ core.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50))
+    password = db.Column(db.String(100))
 
 @app.route('/')
 def hello_world():
