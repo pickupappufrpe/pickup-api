a84ba3db42c6d5ef45f3c1e4cc8e0092d6ee0447
Bruno Santos <brunosantos@protonmail.com>
2019-09-16 08:48:46 -0300

    minor fix

--- core.py
+++ core.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SECCRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
