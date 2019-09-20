71f812fcd33af1039cc9f5deb58a08c065d2a23c
Bruno Santos <brunosantos@protonmail.com>
2019-09-20 11:50:02 -0300

    unit tests configuration

---
+++ tests.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from flask import url_for
+from core import app
+
+
+class RootTests(TestCase):
+    def setUp(self):
+        self.app = app
+        self.app.testing = True
+        self.app_context = self.app.test_request_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+    def test_root_deve_retornar_hello_world(self):
+        request = self.client.get(url_for('hello_world'))
+        self.assertEqual('Hello, World!', request.data.decode())
+
+    def test_root_deve_retornar_status_code_200(self):
+        request = self.client.get(url_for('hello_world'))
+        self.assertEqual(200, request.status_code)
