d5f4e7fd66671f738d75a2a70d451a29a3ae2484
Bruno Santos <brunosantos@protonmail.com>
2019-10-10 19:26:45 -0300

    add user_group validation in login

--- core.py
+++ core.py
@@ -174,21 +174,29 @@ def delete_user(current_user, id):
 @app.route('/login')
 def login():
     auth = request.authorization
-
+    data = request.get_json()
+    asked_group = data['user_group']
     if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
     user = User.query.filter_by(username=auth.username).first()
 
     if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if user.group_id != int(asked_group):
+        return {'message': "Wrong user group!"}
 
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'id' : str(user.id), 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        token = jwt.encode({
+                            'id': str(user.id),
+                            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+                            },
+                           app.config['SECRET_KEY'])
 
-        return {'token' : token.decode('UTF-8')}
+        return {'token': token.decode('UTF-8')}
 
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
 
 @app.route('/person', methods=['POST'])
