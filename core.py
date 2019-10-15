a2a1b56490281b2681455ff7ed889e4d4cff723f
Bruno Santos <brunosantos@protonmail.com>
2019-10-15 14:56:30 -0300

    turn off DEBUG

--- core.py
+++ core.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 
 
