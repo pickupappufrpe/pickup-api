009e1504d6f423336172e5daa71b55e5b159eeaf
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-10-17 23:24:36 -0300

    Update core.py

--- core.py
+++ core.py
@@ -130,7 +130,7 @@ def login():
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
-    result = User.query.filter_by(username=auth.username, id_group= asked_group).first()
+    result = User.query.filter_by(username=auth.username, group_id= asked_group).first()
 
     if not result:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
