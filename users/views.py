62d74643ff8bae47375fa4440f22db87ae902d36
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-10-20 20:31:02 -0300

    update teste login por tipo de usuario

--- users/views.py
+++ users/views.py
@@ -95,13 +95,12 @@ def delete_user(current_user, user_id):
 
 @user.route('/user/full/<user_id>', methods=['GET'])
 @token_required
-def get_user(user_id):
-    id = user_id
+def get_user(current_user, user_id):
     target = db.session.query(
                               User.username,
                               Person.name,
                               Person.surname
-                              ).join(User, User.id == id).join(Person, Person.id == User.person_id).first()
+                              ).join(User, User.id == user_id).join(Person, Person.id == User.person_id).first()
 
     if not target:
         return {'message': 'Sorry!'}
