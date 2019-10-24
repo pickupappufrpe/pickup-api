6d72f1a45f469abb572271a084512e8b396fc5df
Bruno Santos <brunosantos@protonmail.com>
2019-10-24 08:44:24 -0300

    token_required in add_schedule

--- schedules/views.py
+++ schedules/views.py
@@ -1,14 +1,17 @@
 from . import schedule
 
 from flask import request
+from core import token_required
 from .controls import add_schedule_query
 
+
 @schedule.route('/schedule', methods=['POST'])
+@token_required
 def add_schedule():
     data = request.get_json()
     if add_schedule_query(data['spot_id'],
                           data['week_day'],
                           data['opening_time'],
-                          data['closing_time'])
+                          data['closing_time']):
         return {'message': 'Schedule added!'}
 
