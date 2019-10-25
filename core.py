52e3a040d24a76661e108e2255d30cae7ce2cc96
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-10-25 04:39:42 -0300

    Update core.py

--- core.py
+++ core.py
@@ -83,7 +83,7 @@ class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     spot_id = db.Column(db.Integer, db.ForeignKey('spot.id'))
-    image = db.Column(db.LargeBinary)
+    image = db.Column(db.String(10485760))
 
 
 class Schedule(db.Model):
