d5b37fccc59583b6ff0136c34c566b9f5fbd810b
Bruno Santos <brunosantos@protonmail.com>
2019-10-31 14:54:39 -0300

    return error messages

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,7 @@
 from . import photo
 
 from core import db, Photo, User, Spot, token_required, app
-from flask import request, flash, redirect, url_for, send_file
+from flask import request, send_file
 from werkzeug.utils import secure_filename
 import os
 
@@ -19,14 +19,12 @@ def upload_file(current_user, spot_id):
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return {'message': 'No file part'}
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return {'message': 'No selected file'}
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
