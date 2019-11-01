0c5b7d90f2a9dcdc4fa937bf835f9fabce799298
Bruno Santos <brunosantos@protonmail.com>
2019-10-31 23:54:57 -0300

    S3 integration

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,15 @@
 from . import photo
 
 from core import db, Photo, User, Spot, token_required, app
-from flask import request, send_file
+from flask import request, send_file, Response
+import boto3
 import os
 import uuid
+
+S3_KEY = os.environ.get('KEY_ID')
+S3_SECRET = os.environ.get('ACCESS_KEY')
+s3 = boto3.resource('s3', aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+bucket = os.environ.get('BUCKET')
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
@@ -15,7 +21,7 @@ def allowed_file(filename):
 
 @photo.route('/spot/<spot_id>/photo', methods=['POST'])
 @token_required
-def upload_file(current_user, spot_id):
+def upload_spot_photo(current_user, spot_id):
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -27,8 +33,31 @@ def upload_file(current_user, spot_id):
             return {'message': 'No selected file'}
         if file and allowed_file(file.filename):
             filename = str(uuid.uuid4()) + "." + file.filename.rsplit('.', 1)[1].lower()
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            s3.Bucket(bucket).put_object(Key=filename, Body=file)
             new_photo = Photo(spot_id=spot_id, image=filename)
+            db.session.add(new_photo)
+            db.session.commit()
+            return {'message': 'Success!'}
+
+
+@photo.route('/spot/<user_id>/photo', methods=['POST'])
+@token_required
+def upload_user_photo(current_user, user_id):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return {'message': 'No file part'}
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return {'message': 'No selected file'}
+        if file and allowed_file(file.filename):
+            filename = str(uuid.uuid4()) + "." + file.filename.rsplit('.', 1)[1].lower()
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            s3.Bucket(bucket).put_object(Key=filename, Body=file)
+            new_photo = Photo(user_id=user_id, image=filename)
             db.session.add(new_photo)
             db.session.commit()
             return {'message': 'Success!'}
@@ -36,31 +65,38 @@ def upload_file(current_user, spot_id):
 
 @photo.route('/spot/<spot_id>/photo/list', methods=['GET'])
 @token_required
-def get_photo_list(current_user, spot_id):
+def get_spot_photo_list(current_user, spot_id):
     target = Photo.query.filter_by(spot_id=spot_id)
     photo_list = []
     for i in target:
         photo_list.append({i.id: i.image})
-    return {'spots': photo_list}
+    return {'spot_photos': photo_list}
+
+
+@photo.route('/user/<user_id>/photo/list', methods=['GET'])
+@token_required
+def get_user_photo_list(current_user, user_id):
+    target = Photo.query.filter_by(user_id=user_id)
+    photo_list = []
+    for i in target:
+        photo_list.append({i.id: i.image})
+    return {'user_photos': photo_list}
 
 
 @photo.route('/photo/<filename>', methods=['GET'])
 @token_required
 def get_photo_by_filename(current_user, filename):
-    path = app.config['UPLOAD_FOLDER']+'/'+filename
+    # path = app.config['UPLOAD_FOLDER']+'/'+filename
 
-    return send_file(path, mimetype='image/gif')
+    my_bucket = s3.Bucket(bucket)
 
+    file_obj = my_bucket.Object(filename).get()
 
-@photo.route('/spot/<spot_id>/photo', methods=['GET'])
-@token_required
-def get_spot_photo(current_user, spot_id):
-    target = Photo.query.filter_by(spot_id=spot_id).first()
-    filename = app.config['UPLOAD_FOLDER']+'/'+target.image
-    if not target:
-        return {'message': 'No photo found!'}
-
-    return send_file(filename, mimetype='image/gif')
+    return Response(
+        file_obj['Body'].read(),
+        mimetype='image/jpeg',
+        # headers={"Content-Disposition": "attachment;filename={}".format(key)}
+    )
 
 
 @photo.route('/user/<user_id>/photo', methods=['POST'])
@@ -72,14 +108,3 @@ def save_user_photo(current_user):
     db.session.add(new_photo)
     db.session.commit()
     return {'message': 'Image saved!'}
-
-
-@photo.route('/user/<user_id>/photo', methods=['GET'])
-@token_required
-def get_user_photo(current_user, user_id):
-    target = Photo.query.filter_by(user_id=user_id).first()
-
-    if not target:
-        return {'message': 'No photo found!'}
-
-    return {'image': target.image}
