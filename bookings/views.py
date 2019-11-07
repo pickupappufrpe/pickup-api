faf64ec631e59b3c9f1f85eef9e87d01dd0dc7c7
Bruno Santos <brunosantos@protonmail.com>
2019-11-06 22:49:56 -0300

    get_my_bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,7 @@
 from . import booking
 
 from flask import request
-from core import token_required, Booking, User
+from core import token_required, Booking, Spot
 from .controls import add_booking_query
 
 
@@ -28,6 +28,27 @@ def get_spot_bookings(current_user, spot_id):
         bookings_data = {
             'day': b.day,
             'spot_id': b.spot_id,
+            'start_time': str(b.start_time),
+            'end_time': str(b.end_time),
+        }
+
+        output.append(bookings_data)
+
+    return {'bookings': output}
+
+
+@booking.route('/spot/booking/my', methods=['GET'])
+@token_required
+def get_my_bookings(current_user):
+    bookings = Booking.query.filter_by(customer_id=current_user.id)
+
+    output = []
+
+    for b in bookings:
+        spot = Spot.query.filter_by(id=b.spot_id).first()
+        bookings_data = {
+            'day': b.day,
+            'spot_name': spot.name,
             'start_time': str(b.start_time),
             'end_time': str(b.end_time),
         }
