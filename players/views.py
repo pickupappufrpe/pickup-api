268df6287b43524002fd7ccba61566c253f7a589
Bruno Santos <brunosantos@protonmail.com>
2019-11-19 22:20:20 -0300

    minor fix

--- players/views.py
+++ players/views.py
@@ -5,7 +5,7 @@ from core import Player, User, Person, token_required
 
 @player.route('/players', methods=['GET'])
 @token_required
-def get_players():
+def get_players(current_user):
     target = Player.query.join(User, Player.user_id == User.id).\
         join(Person, User.person_id == Person.id).\
         add_columns(Person.name, Person.surname, Player.matches_count, Player.average_rating)
