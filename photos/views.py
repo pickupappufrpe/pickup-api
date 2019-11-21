c0606d1a2b262552a852016cb7964ddca5ba001d
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2019-11-21 00:38:24 -0300

    Update views.py

--- photos/views.py
+++ photos/views.py
@@ -68,7 +68,7 @@ def get_user_photo_list(current_user, user_id):
     target = Photo.query.filter_by(user_id=user_id)
     photo_list = []
     for i in target:
-        photo_list.append({i.id: i.filename})
+        photo_list.append({"filename": i.filename})
     return {'user_photos': photo_list}
 
 
